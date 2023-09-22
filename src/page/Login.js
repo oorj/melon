5f45b497206fe29c954fdeb9c9e1c0693335a7c1
oorj <137370451+oorj@users.noreply.github.com>
2023-09-22 17:43:52 +0900

    Update Login.js

--- src/page/Login.js
+++ src/page/Login.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      <h1>로그인페이지</h1>
+    <div className='login'>
+      <div className="id">
+        <from>
+          <input type="text" placeholder='아이디를 입력해주세요.' />
+        </from>
+      </div>
+      <div className="pass">
+        <from>
+          <input type="text" placeholder='비밀번호를 입력해주세요.' />
+        </from>
+      </div>
+      <button>로그인</button>
     </div>
   )
 }
